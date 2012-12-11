1bc409fba80ac7c1566c48d3849b81a5b572f57d
tengsong <shawnt22@gmail.com>
2012-12-11 10:29:43 +0800

    Initial Commit

---
+++ WFNavigation/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  WFNavigation
+//
+//  Created by 滕 松 on 12-12-11.
+//  Copyright (c) 2012年 shawnt22@gmail.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
